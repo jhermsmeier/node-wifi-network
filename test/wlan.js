728b5367bfc330b2f8468d01072046d0efb58047
Jonas Hermsmeier <jhermsmeier@gmail.com>
2017-05-30 23:30:21 +0200

    test: Increase timeout

--- test/wlan.js
+++ test/wlan.js
@@ -4,10 +4,14 @@ var wifi = require( '..' )
 describe( 'wifi-network', function() {
 
   specify( 'scan()', function( done ) {
+
+    this.timeout( 10e3 )
+
     wifi.scan( function( error, networks ) {
       assert.ifError( error )
       done()
     })
+
   })
 
 })
