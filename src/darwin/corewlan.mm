6a6c4e46595b7653ec7112130bded9fb43bc8caa
Jonas Hermsmeier <jhermsmeier@gmail.com>
2020-04-25 18:58:02 +0200

    src(darwin): Fix segfault on MacOS Catalina
    Fix #6

--- src/darwin/corewlan.mm
+++ src/darwin/corewlan.mm
@@ -1,62 +1,76 @@
+#import <Foundation/Foundation.h>
+#import <CoreWLAN/CoreWLANConstants.h>
+#import <CoreWLAN/CoreWLANTypes.h>
 #import <CoreWLAN/CoreWLAN.h>
+
 #import "../wifi.hpp"
 
-long get_channel_band(CWChannel *channel) {
-  switch( [channel channelBand] ) {
-    case kCWChannelBandUnknown:
-      return 0;
-    case kCWChannelBand2GHz:
-      return 2;
-    case kCWChannelBand5GHz:
-      return 5;
+long get_channel_band( CWChannel *channel ) {
+
+  auto channelBand = channel != nil ?
+    [channel channelBand] : kCWChannelBandUnknown;
+
+  switch( channelBand ) {
+    case kCWChannelBandUnknown: return 0;
+    case kCWChannelBand2GHz: return 2;
+    case kCWChannelBand5GHz: return 5;
+    default: return -1;
   }
+
 }
 
-long get_channel_width(CWChannel *channel) {
-  switch( [channel channelWidth] ) {
-    case kCWChannelWidthUnknown:
-      return 0;
-    case kCWChannelWidth20MHz:
-      return 20;
-    case kCWChannelWidth40MHz:
-      return 40;
-    case kCWChannelWidth80MHz:
-      return 60;
-    case kCWChannelWidth160MHz:
-      return 120;
+long get_channel_width( CWChannel *channel ) {
+
+  auto channelWidth = channel != nil ?
+    [channel channelWidth] : kCWChannelWidthUnknown;
+
+  switch( channelWidth ) {
+    case kCWChannelWidthUnknown: return 0;
+    case kCWChannelWidth20MHz: return 20;
+    case kCWChannelWidth40MHz: return 40;
+    case kCWChannelWidth80MHz: return 60;
+    case kCWChannelWidth160MHz: return 120;
+    default: return -1;
   }
+
 }
 
 std::vector<WifiNetwork> wifi_scan_networks() {
 
   CWChannel *channel;
-  // TODO: Error handling
   NSError *error;
 
   CWWiFiClient *client = [CWWiFiClient sharedWiFiClient];
   CWInterface *interface = [client interface];
 
-  NSSet<CWNetwork *> *networks = [interface scanForNetworksWithSSID:nil error:&error];
+  NSSet<CWNetwork *> *networks = [interface scanForNetworksWithSSID:nil includeHidden:true error:&error];
   std::vector<WifiNetwork> wifi_networks;
 
-  for( CWNetwork *network in networks ) {
+  if( error == nil ) {
+    for( CWNetwork *network in networks ) {
 
-    WifiNetwork wifi_network = WifiNetwork();
-    channel = [network wlanChannel];
+      WifiNetwork wifi_network = WifiNetwork();
+      channel = [network wlanChannel];
 
-    wifi_network.ssid = [[network ssid] UTF8String];
-    wifi_network.bssid = [[network bssid] UTF8String];
-    wifi_network.country_code = [[network countryCode] UTF8String] ?
-      [[network countryCode] UTF8String] : "";
-    wifi_network.beacon_interval = [network beaconInterval];
-    wifi_network.noise = [network noiseMeasurement];
-    wifi_network.rssi = [network rssiValue];
-    wifi_network.channel_number = [channel channelNumber];
-    wifi_network.channel_band = get_channel_band( channel );
-    wifi_network.channel_width = get_channel_width( channel );
+      wifi_network.ssid = [[network ssid] UTF8String] != nil ?
+        [[network ssid] UTF8String] : "";
 
-    wifi_networks.push_back( wifi_network );
+      wifi_network.bssid = [[network bssid] UTF8String] != nil ?
+        [[network bssid] UTF8String] : "";
 
+      wifi_network.country_code = [[network countryCode] UTF8String] != nil ?
+        [[network countryCode] UTF8String] : "";
+
+      wifi_network.beacon_interval = [network beaconInterval];
+      wifi_network.noise = [network noiseMeasurement];
+      wifi_network.rssi = [network rssiValue];
+      wifi_network.channel_number = channel != nil ? [channel channelNumber] : -1;
+      wifi_network.channel_band = get_channel_band( channel );
+      wifi_network.channel_width = get_channel_width( channel );
+
+      wifi_networks.push_back( wifi_network );
+
+    }
   }
 
   return wifi_networks;
